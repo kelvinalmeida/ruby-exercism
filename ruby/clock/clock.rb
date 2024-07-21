1585a26ea30ed5c9df5a864b972a7a87d1c5f09c
Kelvin Santos <klevinsantosdealmeida@gmail.com>
2024-07-21 09:12:21 -0300

    clock

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,17 @@
+=begin
+Write your code for the 'Clock' exercise in this file. Make the tests in
+`clock_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/clock` directory.
+=end
+
+class Clock
+  def initialize(str)
+    @clock = str
+  end
+
+  def to_s
+    "i'm done"
+  end
+end
