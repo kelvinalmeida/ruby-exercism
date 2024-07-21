9ee200a2b542100688fb4c6818c83d53bd34118d
Kelvin Santos <klevinsantosdealmeida@gmail.com>
2024-07-21 08:55:36 -0300

    Luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -15,25 +15,43 @@ module Luhn
       return false
     end
 
-    array = numbers.split(" ")
-    puts array
-    
-    # debug "The value is #{numbers}."
+    array_of_numbers = numbers.split(" ").join.chars.reverse
 
-    numbers.chars.each do |digit|
+    array_of_numbers.each do |char|
+      print "\n#{char}\n"
+      if letter?(char)
+        return false
+      end
+    end
 
-      puts array
+    array_of_numbers = array_of_numbers.each_with_index.map { |num, idx| ((idx + 1) % 2) == 0 ? double_number(num.to_i): num.to_i }
 
-      # if(digit.to_i == 0 )
-      #   raise ArgumentError.new
-      # end
-      
-      # array.push(digit.to_i)
-    end  
+    num_sum = array_of_numbers.sum
+
+    if(num_sum % 10 == 0)
+      print "3 - #{num_sum % 10}\n"
+      return true
+    else
+      print "4 - #{num_sum % 10}\n"
+      return false
+    end
   end
 
-  def self.numeric?(lookAhead)
-    lookAhead =~ /[0-9]/
+  def self.double_number(num)
+    num_d = num.to_i * 2
+
+    if(num_d > 9)
+      return (num_d - 9)
+    else
+      return num_d
+    end
+  end
+
+
+  # checks if it is a letter or a special character
+  def self.letter?(lookAhead)
+    special = "?<>',?[]}{=-)(*&^%$#`~{}"
+    lookAhead =~ /[A-Za-z]/ || lookAhead =~ /[#{special.gsub(/./){|char| "\\#{char}"}}]/
   end
 
 end
