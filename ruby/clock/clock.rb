38db0198299b0c11232bdfbe595e2d67f7e50497
Kelvin Santos <klevinsantosdealmeida@gmail.com>
2024-07-25 15:01:24 -0300

    clock

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -14,14 +14,14 @@ class Clock
 
   def to_s
 
-    if((@hour < 10 and @minute < 10) and (@minute >= 0))
+    if((0...10) === @hour and (0...10) === @minute)
 
       puts "@hour < 10 and @minute < 10"
       "0#{@hour}:0#{@minute}"
 
-    elsif(@hour > 0)
+    else
 
-      puts "@hour > 0"
+      puts "else"
       hour = get_hour(@hour, @minute)
       puts "hour #{hour}"
       minute = get_minute(@minute)
@@ -34,8 +34,8 @@ class Clock
   def get_hour(hour, minute)
 
 
-    if((hour >= 0 and hour < 9) and (minute >= 0))
-
+    if(((0...9) === hour) and (minute >= 0))
+      puts "get hour >>> (hour  === (0...9)) and (minute >= 0)"
       hour += minute < 60 ? 0 : get_the_hour_by_minutes(minute)
 
       "0#{hour}"
@@ -52,23 +52,36 @@ class Clock
 
       ret_hour < 10 ? "0#{ret_hour}" : ret_hour
 
-    elsif((hour > 0) and (minute > -60))
-      ret_hour = hour - 1
-      "0#{ret_hour}"
+    elsif(((0...10) === hour) and (minute > -60))
 
-    # elsif((hour < 0) and (minute > -60))
-    #   ret_hour = hour - 1
-    #   "0#{ret_hour}"
+    hour == 0 ? "23" : "0#{hour - 1}"
+
+    elsif((hour < 0) and (minute >= 0))
+      puts "(hour < 0) and (minute >= 0)"
+
+      ret_hour = 0
+
+      ret_hour += minute > 60 ? 0 : get_the_hour_by_minutes(minute)
+
+      ret_hour += hour.abs < 24 ? (24 + hour) : 24 - (hour.abs % 24)
+
+      # puts hour.abs % 24
+
+      ret_hour < 10 ? "0#{ret_hour}" : ret_hour
     end
   end
 
   def get_minute(minute)
-    if(minute >= 0 and minute < 60)
+    if((0...10) === minute)
+      puts "get_minute (0...10) === minute"
       "0#{minute}"
-    elsif(minute >= 60)
-      minute = (minute % 60)
-      minute < 10 ? "0#{minute}" : minute
+    elsif(minute >= 10)
+      puts "get_minute minute >= 10"
+      minute_ret= 0
+      minute_ret = minute < 60 ? minute : (minute % 60)
+      (0...10) === minute_ret ? "0#{minute_ret}" : "#{minute_ret}"
     elsif(minute >= -60)
+      puts "get_minute minute >= -60"
       ret_min = 60 + minute
       ret_min < 10 ? "0#{ret_min}" : ret_min
     end
@@ -93,4 +106,4 @@ class Clock
 end
 
 
-Clock.new(hour: 1, minute: -40).to_s
+puts Clock.new(hour: -1, minute: 15).to_s
